be608cdc8760d337362bd8ebdaea15c33a80b445
Vilius Poska <vilius@poska.lt>
2017-03-23 14:51:58 +0200

    added shortcut is_iterable, updated validation-sequence.png

--- django_alt/utils/shortcuts.py
+++ django_alt/utils/shortcuts.py
@@ -1,3 +1,5 @@
+import collections
+
 from django.db.models import QuerySet
 from rest_framework import serializers
 
@@ -21,16 +23,26 @@ def invalid(key_or_list, error_or_list):
     raise validation_error_class(make_error(key_or_list, error_or_list))
 
 
+def is_iterable(obj):
+    """
+    Shortcut for checking if object is an iterable.
+    This method *does not* count strings as iterables.
+    :param obj: object to check
+    :return: whether the object is an iterable or not
+    """
+    return not isinstance(obj, str) and isinstance(obj, collections.Iterable)
+
+
 def make_error(key_or_list, error_or_list) -> dict:
     """
     Creates a unified error object.
     :param key_or_list: field name (can be None if error is a non_field one or a list of str)
     :param error_or_list: one or more string representations of the error
     """
-    err = error_or_list if isinstance(error_or_list, list) else [error_or_list]
+    err = error_or_list if is_iterable(error_or_list) else [error_or_list]
     err = list(map(lambda e: (e if e[-1] == '.' or e[-1] == '!' else e + '.') if len(e) else e, err))
     if key_or_list:
-        return {k: err for k in key_or_list} if isinstance(key_or_list, list) else {key_or_list: err}
+        return {k: err for k in key_or_list} if is_iterable(key_or_list) else {key_or_list: err}
     return {'non_field_errors': err}
 
 
