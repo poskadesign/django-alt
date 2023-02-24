77b19e597d100d9ce021b439e56615aa6e644745
Vilius Poska <vpf211@gmail.com>
2023-02-24 17:53:14 -0500

    bring back __legacy__

---
+++ django_alt/__legacy__/django_alt/utils/shortcuts.py
@@ -0,0 +1,226 @@
+import collections
+
+from django.db.models import QuerySet
+from rest_framework import serializers
+
+validation_error_class = serializers.ValidationError
+
+
+def invalid_if(condition, key_or_list, error_or_list):
+    """
+    Shortcut for raising a validation error if a condition is met.
+    :raises: serializers.ValidationError
+    """
+    if condition:
+        invalid(key_or_list, error_or_list)
+
+
+def invalid(key_or_list, error_or_list):
+    """
+    Shortcut for raising a validation error.
+    :raises: serializers.ValidationError
+    """
+    raise validation_error_class(make_error(key_or_list, error_or_list))
+
+
+def is_iterable(obj):
+    """
+    Shortcut for checking if object is an iterable.
+    This method *does not* count strings as iterables.
+    :param obj: object to check
+    :return: whether the object is an iterable or not
+    """
+    return not isinstance(obj, str) and isinstance(obj, collections.Iterable)
+
+
+def make_error(key_or_list, error_or_list) -> dict:
+    """
+    Creates a unified error object.
+    :param key_or_list: field name (can be None if error is a non_field one or a list of str)
+    :param error_or_list: one or more string representations of the error
+    """
+    err = error_or_list if is_iterable(error_or_list) else [error_or_list]
+    err = list(map(lambda e: (e if e[-1] == '.' or e[-1] == '!' else e + '.') if len(e) else e, err))
+    if key_or_list:
+        return {k: err for k in key_or_list} if is_iterable(key_or_list) else {key_or_list: err}
+    return {'non_field_errors': err}
+
+
+def if_in(key, container, func_true=None, default=None):
+    """
+    Checks if given key on the container exists and executes.
+    func_true or func_false with each corresponding value based on the outcome.
+    :param key: the key to search
+    :param container: the container to search in
+    :param func_true: callable that is called on success: func_true(container[key])
+    :param default: default value to set if key does not exist in the container
+    :return: {bool} whether the key exists
+    """
+    if key in container and func_true is not None:
+        container[key] = func_true(container[key])
+        return True
+    elif default is not None:
+        container[key] = default
+        return False
+
+
+def queryset_has_many(queryset) -> bool:
+    """
+    Checks whether a given query product is iterable.
+    :param queryset: Django QuerySet object
+    :return: {bool}
+    """
+    return isinstance(queryset, QuerySet) or hasattr(queryset, '__iter__')
+
+
+def coal(obj, fallback):
+    """
+    Inspired by C# null-coalescing operator:
+    C# --> not_null = obj ?? new Object()
+    Py --> not_null = coal(obj, Object())
+    Also returns fallback upon a KeyError
+    """
+    try:
+        return obj if obj is not None else fallback
+    except KeyError:
+        return fallback
+
+
+def coald(obj, fallback_lambda):
+    """
+    A deferred variation of the null-coalescing operator.
+    Returns obj if it is defined, execution result of a parametress lambda (fallback_lambda) otherwise.
+    """
+    return obj if obj is not None else fallback_lambda()
+
+
+def if_all_in(keys, container, func_true=None):
+    """
+    checks if all given keys are in the container and
+    executes func_true for each of the corresponding values
+    :param keys: iterable of keys to find
+    :param container: the container to search in
+    :param func_true: callable that is called for each value on success: func_true(container[key])
+    :return: whether the keys exist
+    """
+    if all(key in container for key in keys):
+        for key in keys:
+            container[key] = func_true(container[key])
+        return True
+    return False
+
+
+def if_any_in(keys, container, func_true=None):
+    """
+    checks if any given keys ar in the container and
+    executes func_true for each of the corresponding values of the existing keys
+    :param keys: iterable of keys to find
+    :param container: the container to search in
+    :param func_true: callable that is called for each value on success: func_true(container[key])
+    :return: whether any of the keys exist
+    """
+    found_key = False
+    for key in keys:
+        if key in container:
+            container[key] = func_true(container[key])
+            found_key = True
+    return found_key
+
+
+def first_defined(*args):
+    """
+    Finds the first argument that is not None
+    :param args: arguments to check for "defineness"
+    :return:
+    """
+    for arg in args:
+        if arg is not None:
+            return arg
+    return None
+
+
+def prohibited(key: str, container: dict = None):
+    """
+    shortcut for raising a validation error when a prohibited field is in the container.
+    :param container: container to search
+    :param key: existing key
+    :raises: serializer.ValidationError
+    """
+    invalid_if(container and key in container, key, 'This field cannot be present.')
+
+
+def prohibited_any(keys: tuple, container: dict = None):
+    """
+    shortcut for raising a validation error when any of the given fields exist in the container.
+    :param container: container to search
+    :param key: existing key
+    :raises: serializer.ValidationError
+    """
+    intersection = set(keys).intersection(container.keys())
+    if container and len(intersection) > 0:
+        invalid(intersection, 'This field cannot be present.')
+
+
+def required(key: str, container: dict = None):
+    """
+    shortcut for raising a validation error about a missing required field.
+    :param container: container to search
+    :param key: missing key
+    :raises: serializer.ValidationError
+    """
+    if container and key in container:
+        return container[key]
+    invalid(key, 'This field is required.')
+
+
+def required_all(keys: list, container: dict = None):
+    """
+    shortcut for raising a validation error if any of the keys are missing in the container.
+    :param keys: keys that are required in the container
+    :param container: container to search
+    :raises: serializer.ValidationError
+    """
+    if container:
+        difference = set(keys).difference(container.keys())
+        if len(difference):
+            invalid(difference, 'This field is required.')
+
+
+def try_cast(typ, value):
+    """
+    Attempts to cast value to a given type.
+    :param typ: type to cast to
+    :param value: value to be casted
+    :return: casted value if cast was successful, otherwise None
+    """
+    try:
+        return typ(value)
+    except (ValueError, TypeError):
+        return None
+
+
+def valid_if(condition, key_or_list, error_or_list):
+    """
+    Shortcut for raising a validation error if a condition is not met.
+    Think about this function as an assertion (i.e. this condition must be met).
+    :raises: serializers.ValidationError
+    """
+    if not condition:
+        invalid(key_or_list, error_or_list)
+
+
+"""
+Alias of `valid_if`.
+Used in Design by Contract methodology to denote
+that a callable asserts that a given condition is true
+before executing any code
+"""
+expects = valid_if
+
+"""
+Alias of `valid_if`.
+Used in Design by Contract methodology to denote
+that a callable asserts that a given condition is true
+after its code block is executed
+"""
+ensures = valid_if
---
+++ django_alt/__legacy__/django_alt_tests/tests/test_endpoints.py
@@ -0,0 +1,287 @@
+from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
+
+from django_alt.__legacy__.django_alt.abstract.endpoints import MetaEndpoint
+from django_alt.__legacy__.django_alt.endpoints import Endpoint
+from django_alt.__legacy__.django_alt_tests.conf.endpoints import ModelASerializer, ModelAEndpoint1
+from django_alt.__legacy__.django_alt_tests.conf.models import ModelA
+
+
+class MetaEndpointTests(TestCase):
+    def test_transform_config(self):
+        def are_equal(c1, c0):
+            c1 = MetaEndpoint.transform_config_shorthands(c1)
+            self.assertDictEqual(c0, c1)
+
+        are_equal({
+            'get': None
+        }, {
+            'get': {}
+        })
+
+        are_equal({
+            'get, post': None
+        }, {
+            'get': {},
+            'post': {}
+        })
+
+        are_equal({
+            ' get,post, put': None
+        }, {
+            'get': {},
+            'post': {},
+            'put': {},
+        })
+        are_equal({
+            ' get,post': {
+                'q': 1
+            }
+        }, {
+            'get': {'q': 1},
+            'post': {'q': 1},
+        })
+        are_equal({
+            ' get,post': {
+                'q': 1
+            },
+            'post': {
+                'z': 2
+            }
+        }, {
+            'get': {'q': 1},
+            'post': {'q': 1, 'z': 2},
+        })
+        are_equal({
+            ' get': {
+                'q': 1
+            },
+            'get': {
+                'z': 2
+            }
+        }, {
+            'get': {'q': 1, 'z': 2},
+        })
+        are_equal({
+            ' get, get': {
+                'q': 1
+            },
+            'post,get': {
+                'z': 4
+            }
+        }, {
+            'get': {'q': 1, 'z': 4},
+            'post': {'z': 4}
+        })
+
+    def test_unknown_http_method(self):
+        with self.assertRaises(AssertionError):
+            class MyEndpoint(Endpoint):
+                serializer = ModelASerializer
+                config = {'get,foo': None}
+
+
+class EndpointTests(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+
+    def test_required_fields(self):
+        with self.assertRaises(AssertionError):
+            class MyEndpoint(Endpoint):
+                pass
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint1(Endpoint):
+                config = {
+                    'foo': {
+                        'query': None
+                    }
+                }
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint2(Endpoint):
+                config = {
+                    'get': {
+                        'query': None
+                    }
+                }
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint3(Endpoint):
+                config = {
+                    'get': {
+                        'filters': {
+                            'y': lambda x, y: (x, y)
+                        }
+                    }}
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint4(Endpoint):
+                config = {
+                    'get': {
+                        'query': lambda x, y: (x, y),
+                        'filters': {
+                            'x': lambda x, y: (x, y)
+                        }
+                    }}
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint5(Endpoint):
+                serializer = ModelASerializer
+                config = {
+                    'get': {
+                        'query': lambda x, y: (x, y),
+                        'filters': lambda x, y: (x, y)
+                    }}
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint7(Endpoint):
+                serializer = ModelASerializer
+                config = {
+                    'get': {
+                        'query': {'something_strange'},
+                        'filters': {
+                            'x': lambda x, y: (x, y)
+                        }
+                    }}
+
+        with self.assertRaises(AssertionError):
+            class MyEndpoint8(Endpoint):
+                serializer = ModelASerializer
+                config = {
+                    'delete': None
+                }
+
+        class MyEndpoint6(Endpoint):
+            serializer = ModelASerializer
+            config = {
+                'get': {
+                    'query': lambda x, y: (x, y),
+                    'filters': {
+                        'x': lambda x, y: (x, y)
+                    }
+                },
+                'delete': {
+                    'query': lambda x, y: (x, y)
+                }}
+
+    def test_basic_get(self):
+        self.assertTrue(hasattr(ModelAEndpoint1, 'as_view'))
+        resp = self.client.get(reverse('e1'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(resp.data, [])
+
+        ModelA.objects.create(field_1='aa', field_2=5)
+        resp = self.client.get(reverse('e1'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertDictEqual(dict(resp.data[0]), {'id': 1, 'field_1': 'aa', 'field_2': 5})
+
+    def test_method_not_supported(self):
+        self.assertEqual(self.client.post(reverse('e1')).status_code, 405)
+        self.assertEqual(self.client.patch(reverse('e1')).status_code, 405)
+        self.assertEqual(self.client.put(reverse('e1')).status_code, 405)
+        self.assertEqual(self.client.delete(reverse('e1')).status_code, 405)
+
+    def test_raises_validation_error(self):
+        ModelA.objects.create(field_1='aa', field_2=5)
+        resp = self.client.get(reverse('e2'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertDictEqual(dict(resp.data[0]), {'id': 1, 'field_1': 'aa', 'field_2': 5, 'extra': 'this is an extra'})
+
+        ModelA.objects.create(field_1='aa', field_2=5)
+        resp = self.client.get(reverse('e2'))
+        self.assertEqual(resp.status_code, 400)
+        self.assertDictEqual(dict(resp.data), {'error': ['msg.']})
+
+    def test_filtered_get(self):
+        ModelA.objects.create(field_1='aaa', field_2=5)
+        ModelA.objects.create(field_1='aaa', field_2=6)
+        ModelA.objects.create(field_1='aca', field_2=10)
+        ModelA.objects.create(field_1='ada', field_2=10)
+        ModelA.objects.create(field_1='aea', field_2=10)
+        ModelA.objects.create(field_1='aaa', field_2=10)
+
+        resp = self.client.get(reverse('e3') + '?filter1=aaa')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.data), 3)
+        for d in resp.data:
+            self.assertEqual(d['field_1'], 'aaa')
+
+        resp = self.client.get(reverse('e3') + '?filter2=10')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.data), 4)
+        for d in resp.data:
+            self.assertEqual(d['field_2'], 10)
+
+        resp = self.client.get(reverse('e3') + '?filter1=aaa&filter2=10')
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.data), 1)
+        for d in resp.data:
+            self.assertEqual(d['field_1'], 'aaa')
+            self.assertEqual(d['field_2'], 10)
+
+    def test_get_precondition_permission_check(self):
+        resp = self.client.get(reverse('e4'))
+        self.assertEqual(resp.status_code, 401)
+
+    def test_get_postcondition_permission_check(self):
+        resp = self.client.get(reverse('e5'))
+        self.assertEqual(resp.status_code, 401)
+
+    def test_basic_post(self):
+        resp = self.client.post(reverse('e6'), {'field_1': 'aaa', 'field_2': 3})
+        self.assertEqual(resp.status_code, 201)
+        self.assertDictEqual(resp.data, {'id': 1, 'field_1': 'aaa', 'field_2': 3})
+        self.assertEqual(ModelA.objects.count(), 1)
+        self.assertEqual(ModelA.objects.all().first().field_1, 'aaa')
+        self.assertEqual(ModelA.objects.all().first().field_2, 3)
+
+    def test_basic_patch(self):
+        ModelA.objects.create(field_1='aaa', field_2=3)
+        resp = self.client.patch(reverse('e6'), {'field_1': 'bbb'})
+        self.assertEqual(resp.status_code, 200)
+        self.assertDictEqual(resp.data, {'id': 1, 'field_1': 'bbb', 'field_2': 3})
+        self.assertEqual(ModelA.objects.count(), 1)
+        self.assertEqual(ModelA.objects.all().first().field_1, 'bbb')
+        self.assertEqual(ModelA.objects.all().first().field_2, 3)
+
+    def test_basic_delete(self):
+        ModelA.objects.create(field_1='aca', field_2=10)
+        ModelA.objects.create(field_1='ada', field_2=10)
+        ModelA.objects.create(field_1='aea', field_2=10)
+
+        resp = self.client.delete(reverse('e7'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(ModelA.objects.count(), 2)
+
+    def test_delete_many(self):
+        ModelA.objects.create(field_1='aca', field_2=10)
+        ModelA.objects.create(field_1='ada', field_2=10)
+        ModelA.objects.create(field_1='aea', field_2=10)
+
+        resp = self.client.delete(reverse('e8'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(ModelA.objects.count(), 0)
+
+    def test_fields_from_url(self):
+        resp = self.client.post(reverse('e9', kwargs={'field_1': 'abc', 'field_2': 123}))
+
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(ModelA.objects.count(), 1)
+        self.assertEqual(ModelA.objects.first().field_1, 'abc')
+        self.assertEqual(ModelA.objects.first().field_2, 123)
+
+    def test_fields_from_url_override_args(self):
+        resp = self.client.post(reverse('e9', kwargs={'field_1': 'abc', 'field_2': 123}), {
+            'field_1': 'def'
+        })
+
+        self.assertEqual(resp.status_code, 201)
+        self.assertEqual(ModelA.objects.count(), 1)
+        self.assertEqual(ModelA.objects.first().field_1, 'abc')
+        self.assertEqual(ModelA.objects.first().field_2, 123)
+
+    def test_fields_from_url_nonexistent_args(self):
+        with self.assertRaises(AssertionError) as e:
+            self.client.post(reverse('e10', kwargs={'field_1': 'abc', 'field_2': 123}), )
